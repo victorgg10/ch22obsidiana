17ec690a15aefc34112cbaf03d6cd09c81109df2
philter15 <philix.1115@gmail.com>
2023-03-02 18:09:10 -0600

    trabajando back

--- js/piedraNacInfo.js
+++ js/piedraNacInfo.js
@@ -1,32 +1,108 @@
-import { piedrasPorMes } from "./piedraNac.js";
+//import { piedrasPorMes } from "./piedraNac.js";
+const piedrasPorMes = `
+{
+    "Enero" : {
+        "mes" : "Enero",
+        "piedra" : "Piedra granate",
+        "descripcion" : "La piedra granate es asociado directamente con la fe, la constancia para alcanzar los objetivos y el amor tanto de pareja como el amor en familia y amigos. Debido a este significado se considera como un excelente regalo que simboliza amistad y confianza. <br>El granate es un mineral formador de roca que pertenece al grupo de los nesosilicatos, y aunque la gente piensa que solo aparece en color rojo lo cierto es que se forma en varios ambientes geológicos y ocurre en gran variedad de tonalidades. Por ejemplo: verde (tsavorita y demantoide), naranja (espesartina y hessionita), púrpura (rodolita), amarillo (malí y topazolita), rosa. (malaya), rojo (almandino y piropo), negro (melanita).",
+        "img" : ""
+    },
+    "Febrero" : {
+        "mes" : "Febrero",
+        "piedra" : "Piedra amatista",
+        "descripcion" : "La piedra amatista es una de las variedades más famosas del cuarzo. Es un cristal mineral que forma parte del extenso grupo de los tectosilicatos y se caracteriza por presentar un hermoso color morado o púrpura. Esta piedra de nacimiento ha sido utilizada desde la antigüedad, incluso fue muy famosa en la antigua Roma, donde se le asociaba con la energía del fuego, la creatividad, la fidelidad, la paz y la realeza. También se sabe que los antiguos griegos usaban la piedra amatista con el objetivo de protegerse de los efectos de la embriaguez.",
+        "img" : ""
+    },
+    "Marzo" : {
+        "mes" : "Marzo",
+        "piedra" : "Piedra aguamarina",
+        "descripcion" : "La piedra aguamarina es la piedra de nacimiento del mes de marzo. Esta gema se llamó así debido a que representa a los bastos océanos y al cielo en sus tonalidades. Mineralógicamente es un ciclosilicato y un mineral perteneciente el grupo del berilo, junto con la esmeralda y la morganita. Debido a sus bellos colores en azul pálido y azul profundo los marineros usaban esta gema como amuleto de protección contra los peligros del mar, es decir creían era un regalo de las sirenas. La piedra aguamarina significa entonces coraje, salud y confianza. Además, se le asocia en la actualidad con la espiritualidad, con la paz mental y la reducción del estrés. Finalmente, también simboliza vitalidad, juventud, paz interior y felicidad.",
+        "img" : ""
+    },
+    "Abril" : {
+        "mes" : "Abril",
+        "piedra" : "Piedra diamante",
+        "descripcion" : "La piedra de nacimiento del mes de abril es el diamante. Para nadie es un secreto que el diamante es una de las piedras preciosas más codiciadas y caras del mundo. Esto es debido a que es una gema muy resistente, rara pero muy bella. Mineralógicamente se considera como un mineral nativo compuesto solo de carbono (C), tiene la dureza más alta de los minerales, siendo esta de 10 en la escala de Mohs. Aunque se cree que los diamantes naturalmente son transparentes, lo cierto es que esta gema también puede presentarse en múltiples colores como el rojo, azul, verde, negro y púrpura. Debido a estas características el diamante simboliza o significa fortaleza y también se le asocia con la autoestima, poder, confianza y convicción. Además, de acuerdo con su color puede significar, amor, redención, espiritualidad y mucho más.",
+        "img" : ""
+    },
+    "Mayo" : {
+        "mes" : "Mayo",
+        "piedra" : "Piedra esmeralda",
+        "descripcion" : "La piedra de nacimiento del mes de mayo es la esmeralda. Esta gema se caracteriza por presentar un color verde intenso que imita al verde de la naturaleza y de los extensos bosques de nuestro planeta, por tal razón se le asocia directamente con las energías fuertes de la madre tierra. mAdemás, debido a su belleza y tonalidades verdes intensas se considera que es una piedra capaz de atraer la prosperidad, estabilidad financiera y se dice que puede brindar paz mental y emocional a sus dueños. Mineralógicamente la piedra esmeralda pertenece al grupo de los ciclosilicatos y al grupo de los berilos junto a la piedra morganita y aguamarina.",
+        "img" : ""
+    },
+    "Junio" : {
+        "mes" : "Junio",
+        "piedra" : "Piedra alejandrita",
+        "descripcion" : "Las piedras de nacimiento de junio son la piedra alejandrita y las perlas. La alejandrita es un mineral que pertenece al grupo de los óxidos y crisoberilos, suele tener tonalidades verdosas, sin embargo, su belleza radica en que suele cambiar de color depende del ángulo de la luz a tonalidades verdes azuladas. La piedra alejandrita se cree es especial porque despierta la vitalidad del cuerpo y la mente, es decir que potencia la fuerza de voluntad que ayuda a alcanzar nuestros objetivos.",
+        "img" : ""
+    },
+    "Julio" : {
+        "mes" : "Julio",
+        "piedra" : "Piedra rubí",
+        "descripcion" : "La piedra de nacimiento del mes de julio es el rubí. Este mineral pertenece al grupo de los óxidos y es parte del grupo de las piedras de corindón, junto con la piedra zafiro, sin embargo, a diferencia del zafiro el rubí se presenta con bellas tonalidades rojizas debido a impurezas de cromo. La piedra rubí significa coraje, victoria y éxito, por tal razón, se piensa que atare buena suerte, fortalece la positividad, atrae buenos negocios y te brinda la fuerza para acabar con tus actividades y alcanzar tus objetivos. Además, debido a su tonalidad muchas personas creen que logra atraer el amor de pareja y fortalecer el amor de familia o amigos.",
+        "img" : ""
+    },
+    "Agosto" : {
+        "mes" : "Agosto",
+        "piedra" : "Piedra peridoto",
+        "descripcion" : "La piedra de nacimiento del mes de agosto es el peridoto. Esta gema pertenece al grupo de los nesosilicatos y básicamente es una variedad del olivino verde. Es una gema que se presenta con un color verde oscuro y se caracteriza por ser traslúcida. La piedra peridoto fue bastante famosa y codiciada en Egipto donde se le conocía como la gema del sol. Por lo tanto, se cree que es capaz de atraer la felicidad y ayudarte a encontrar solución para tus problemas. Además, se dice que libera la mente brindando paz mental y alejando los pensamientos negativos. También las personas la usan como piedra protectora ante las malas energías.",
+        "img" : ""
+    },
+    "Septiembre" : {
+        "mes" : "Septiembre",
+        "piedra" : "Piedra zafiro",
+        "descripcion" : "La piedra de nacimiento del mes de septiembre es la piedra zafiro. Esta gema pertenece al grupo de los óxidos y al igual que el rubí forma parte del grupo del corindón. Sin embargo, la piedra zafiro se presenta en varias tonalidades de color azul. Esta gema es una de las más codiciadas del mundo y sus tonalidades azules se deben a impurezas de hierro y titanio en su composición química. Desde siempre esta piedra ha simbolizado la paz, la calma y la serenidad similar a lo que significan los océanos y el bello cielo azul. También muchas personas la utilizan para superar la timidez, aumentar la tolerancia y la introspección, debido a que es símbolo de espiritualidad.",
+        "img" : ""
+    },
+    "Octubre" : {
+        "mes" : "Octubre",
+        "piedra" : "Piedra turmalina u ópalo",
+        "descripcion" : "La piedra de nacimiento del mes de octubre es la turmalina y también el ópalo. La turmalina es un mineral bastante variado debido a que se presenta en una infinidad de tonalidades, mineralógicamente pertenece al grupo de los ciclosilicatos. La turmalina dependiendo de sus colores y variedades significa paz, amor, esperanza, vitalidad, armonía, autoestima, y un sin finde emociones. La piedra ópalo es un mineraloide compuesto por sílice amorfo bastante famoso que igualmente se presenta en varios tipos y variedades y básicamente está asociado con la espontaneidad, el amor, la pasión y la autoestima. Sin embargo, de acuerdo con su color y forma simbolizará emociones diferentes.",
+        "img" : ""
+    },
+    "Noviembre" : {
+        "mes" : "Noviembre",
+        "piedra" : "Piedra cuarzo citrino",
+        "descripcion" : "La piedra de nacimiento del mes de noviembre es el cuarzo citrino. Este cristal es una variedad del cuarzo y por lo tanto pertenece al grupo de los tectosilicatos. Es una piedra bastante rara que solo aparece en vetas hidrotermales asociadas con el metal precioso oro y en pegmatitas. Este cristal mineral es bastante famoso debido a que se cree tiene la capacidad de atraer la felicidad y la abundancia hacia el hogar y vida de las personas que lo poseen. Además, es famosa como piedra protectora contra las energías tóxicas y negativas del ambiente y de las demás personas.",
+        "img" : ""
+    },
+    "Diciembre" : {
+        "mes" : "Diciembre",
+        "piedra" : "Piedra tanzanita o piedra turquesa",
+        "descripcion" : "La piedra de nacimiento del mes de diciembre es la tanzanita y la piedra turquesa principalmente. La tanzanita es una gema que pertenece al grupo de los sorosilicatos y se caracteriza por presentarse en un intenso color azul y morado. Además, este mineral es extremadamente raro e incluso llega a ser más valioso que el diamante. Típicamente está asociada con la paz y la tranquilidad mental. Así mismo se le han atribuido varios poderes energéticos y curativos. La piedra turquesa en cambio es mineral o gema que pertenece al grupo de los fosfatos. Esta piedra se presenta con bellas tonalidades verdes y básicamente las personas creen que es un talismán para atraer la buena suerte, de hecho, en la cultura turca la llaman el talismán del jinete. También en otras culturas como la griega se dice que es una piedra que logra atraer el amor.",
+        "img" : ""
+    }
+}
+`;
 
 
 
-
-const element = document.querySelector(".mes").addEventListener('click',respuestaPorMes);
-
-
-const mesNacimiento = () => {
+function seleccionMes () => {
+    console.log(resumenPiedraNacimiento.Enero.mes);
     
-    const resumenPiedraNacimiento = JSON.parse(piedrasPorMes);
-    fetch(resumenPiedraNacimiento)
-        .then(response => {
-            return response.json();
-        })
-        .catch (error => console.error("Error",error));
-        respuestaPorMes();
+    console.log(resumenPiedraNacimiento.Enero.mes);
+    element = document.getElementsByClassName(".mes").innerHTML;
+    const doc = document.getElementById("piedra");
+    doc.innerHTML = "ya estas ";
+    //element.addEventListener('click', resumenPiedraNacimiento );
+    console.log("ya estas " + datosPiedra.mes);
+    
 }
 
 
+ const resumenPiedraNacimiento = () =>{
+    JSON.parse(piedrasPorMes);
+    //console.log(resumenPiedraNacimiento.Enero.mes);
+    console.log("estas activo");
+}
+/* 
 const respuestaPorMes = () => {
     const clave = element.innerHTML;
 
     document.getElementById(clave).innerHTML = (mesNacimiento.clave.piedra);
 
 
+}  */
 
-
-
-}
-
-console.log(resumenPiedraNacimiento);
+//console.log(element);
